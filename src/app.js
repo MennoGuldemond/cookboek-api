5b6861bf8c49d9517e63c68fbc817f175cca4462
Menno Guldemond <mennoguldemond@live.nl>
2023-12-26 19:25:12 +0100

    fix static files middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv'
 import express from 'express'
 import session from 'express-session'
 import cors from 'cors'
+import path from 'path'
+import * as url from 'url'
 import { serveSwagger } from './swagger.js'
 import { userRouter } from './routes/user.routes.js'
 import { recipeRouter } from './routes/recipe.routes.js'
@@ -9,14 +11,16 @@ import { imageRouter } from './routes/image.routes.js'
 import { categoryRouter } from './routes/category.routes.js'
 import { likeRouter } from './routes/like.routes.js'
 
+const __dirname = url.fileURLToPath(new URL('../', import.meta.url))
+
 const port = process.env.PORT || 3000
 const app = express()
+app.use('/public', express.static(path.join(__dirname, 'public')))
 serveSwagger(app)
 dotenv.config()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
 app.use(
   session({
     resave: false,
@@ -27,6 +31,10 @@ app.use(
 
 app.listen(port, () => {
   console.log(`App running on ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile('./public/dist/index.html', { root: __dirname })
 })
 
 // ROUTES
